826d8f35a86d2dbd11ebc0253a71feb115052b25
Tabby Toney <tabbytoney@gmail.com>
2020-12-15 20:10:28 -0600

    added browser navigation test

--- tests/browser.nav.js
+++ tests/browser.nav.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer')
 
-describe('My First Puppeteer Test', () => {
-    it('should launch the browser', async function () {
+describe('Second Test File', () => {
+    it('Navigating via browser', async function() {
         const browser = await puppeteer.launch({
             headless: false,  //headless false means it'll open a physical browser
             sloMo: 1000,  //slows test down by this many miliseconds
@@ -9,11 +9,13 @@ describe('My First Puppeteer Test', () => {
         })  //opens browser with dev tools open
         const page = await browser.newPage() //connects the page that results from browser starting to puppeteer
         await page.goto('http://example.com') //telling page to to visit this url
-        await page.waitFor(3000) //pauses for three seconds 
-        await page.waitForSelector('h1') //R click, inspect an element. There's an <h1> so we tell it to find this on the page. If yes, it moves on - if none found it throws error
-        await page.reload() //refreshes the browser
-        await page.waitFor(3000)
+        await page.waitForSelector('h1') //for some reason, the waitFor and waitForSelector doesn't work with goBack and goForward
+        await page.goto('https://dev.to/') //second website to go to
+        await page.waitForSelector('#body-styles') //this checks for an id on the second page. Id needs #
+        await page.goBack() //navigate back arrow in the browser
         await page.waitForSelector('h1')
+        await page.goForward() //navigate forward arrow in the browser
+        await page.waitForSelector('#body-styles') 
         await browser.close() //must close browser at end of test or you'll have to do it manually
     })
 })
